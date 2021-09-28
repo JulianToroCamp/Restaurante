cd31844d7be6d82ba1ba07604ab30208adcce4c3
Jhon Loaiza <Jaloaizac@utp.edu.co>
2021-09-28 07:13:07 -0500

    edi del produco

--- js/app-producto.js
+++ js/app-producto.js
@@ -8,6 +8,14 @@ function mostrar_formulario(dato) {
     case 2:
       document.getElementById("insertar").style.display = "none";
       break;
+    case 3:
+			document.getElementById('insertar').style.display = "block";
+			document.getElementById('guardar').style.display = "none";
+			document.getElementById('editar').style.display = "block";
+			break;
+		case 4:
+			document.getElementById('formproducto').style.display = "none";
+			break;
   }
 }
 
@@ -50,7 +58,7 @@ async function leerproducto() {
 		      <tr>
 		       	 	<td>${doc.data().Nombre}</td>
 						  <td>$ ${doc.data().ValorCompra} Cop</td>
-						  <td><button onclick="">Editar</button></td>
+						  <td><button onclick="recibirdatos('${doc.id}','${doc.data().Nombre}','${doc.data().ValorCompra}')">Editar</button></td>
 						  <td><button onclick="eliminarproducto('${doc.id}')">Borrar</button></td>
 					</tr>`;
       }
@@ -76,3 +84,61 @@ function eliminarproducto(id) {
     console.log("no se borro");
   }
 }
+
+//editar producto
+
+async function recibirdatos(id, nombre, valorCompra) {
+	document.getElementById('llave').value = id;
+	document.getElementById('nombre').value = nombre;
+	document.getElementById('valorCompra').value = valorCompra;
+	
+	mostrar_formulario(3);	
+}
+
+function editarproducto() {
+	
+	var id = document.getElementById('llave').value;
+	var nombre = document.getElementById('nombre').value;
+	var valorCompra = document.getElementById('valorCompra').value;
+
+	
+	//edita la información
+	var productoRef = db.collection("productos").doc(id);
+
+	// Set the "capital" field of the city 'DC'
+	return productoRef.update({
+    Nombre: nombre,
+    ValorCompra: valorCompra,
+	})
+	.then(() => {
+	    console.log("El registro se guardo");
+	    registroExitoso ();	
+		 document.getElementById('nombre').value = "";
+		 document.getElementById('valorCompra').value = "";
+		 
+	
+	    mostrar_formulario(2);
+	})
+	.catch((error) => {
+	    // The document probably doesn't exist.
+	    console.error("Error editando el documento: ", error);
+	    alert("Error editando el documento: ", error)
+	});
+	
+	
+}
+
+
+
+function registroExitoso() {
+  Swal.fire({
+    icon: "success",
+    title: "Producto editado!",
+    text: "Tú registro se ha actualizado en la base de datos!",
+  }).then(function () {
+    
+  });
+}
+
+
+
